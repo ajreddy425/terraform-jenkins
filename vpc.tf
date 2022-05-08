a01f5b12afa25e21b64ef9ab6181557375d4627f
ajreddy425 <82323282+ajreddy425@users.noreply.github.com>
2022-05-08 06:22:51 +0530

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main" {             // resource "resource_type" "logical_name"
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "terraform-vpc-7am-${terraform.workspace}"
-  }
-}
